b50584c88f1628c8096955c4f97c01ae9c165e18
nacl <vitus.tseng@gmail.com>
2024-11-21 18:45:27 +0800

    add gitignore

--- mp4Paser.py
+++ mp4Paser.py
@@ -21,7 +21,7 @@ def image_parser(video_path: str, fps: int):
         if not ret:
             break
 
-        if frame_count % fps == 0:
+        if frame_count % int(fps / 60) == 0:
             frame_path = os.path.join(output_dir, f'{frame_count:04d}.png')
             cv2.imwrite(frame_path, frame)
             # ascii_art = ascii_parser(frame)
@@ -39,10 +39,11 @@ def main():
     video_path = input()
     print("please input the fps you want")
     fps = int(input())
+    frame_rate = int(fps / 60)
     frame_count = image_parser(video_path, fps)
     ascii_arts = []
-    for i in range(0, int(frame_count/fps)):
-        frame_path = os.path.join('output_frames', f'{i*fps:04d}.png')
+    for i in range(0, int(frame_count/frame_rate)):
+        frame_path = os.path.join('output_frames', f'{i*frame_rate:04d}.png')
         ascii_art = ascii_parser(frame_path)
         ascii_arts.append(ascii_art)
 
